37ee1494de2fd473b8ce3131924fc9e7e7a25ab4
gta126 <gta126705@hotmail.com>
2013-04-27 12:44:17 +0200

    shared memory

---
+++ common/common.h
@@ -0,0 +1,19 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#define TAILLE_NOM 20
+#define MAX_JOUEUR 4
+#define SHM_KEY 10101
+
+struct game
+{
+	int nbrJoueur;
+	int score[MAX_JOUEUR];
+	char nom[MAX_JOUEUR][TAILLE_NOM];
+};
+
+#endif
+
