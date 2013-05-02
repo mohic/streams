0459a91e7cacfe2d2afe937d3329c21e47d1b1ba
Azhakyn <azhakyn@live.fr>
2013-05-02 11:18:01 +0200

    gestion des messages server

--- server/game.c
+++ server/game.c
@@ -7,6 +7,8 @@
 
 #include "game.h"
 
+int tuilePlacee[MAX_JOUEUR];
+
 int traiterMessage(int sckClient, char *message, game *g, int joueur, int semid) {
 	int i;
 	if (strlen(message) <= 0)
@@ -15,17 +17,32 @@ int traiterMessage(int sckClient, char *message, game *g, int joueur, int semid)
 	switch(message[0]) {
 		case '1':
 			down(semid);
-
-			for (i = 0; i < strlen(message) - 2; i++)
-				g->nom[joueur][i] = message[i+2];
-				
-			g->nbrJoueur++;
+				for (i = 0; i < strlen(message) - 2; i++)
+					g->nom[joueur][i] = message[i+2];
+				g->score[joueur] = -1;	
+				g->nbrJoueur++;
+				printf ("Nom du joueur: %s\n", g->nom[joueur]);
 			up(semid);
-			
-			printf ("Nom du joueur: %s\n", g->nom[joueur]);
 			break;
+		
+		case '3':
+			down(semid);
+				tuilePlacee[joueur] = 1;
+				printf("Le joueur %s a placé sa tuile\n", g->nom[joueur]);
+			up(semid);
+			break;
+		
+		case '5':
+			down(semid);
+				message += 2;
+				g->score[joueur] = atoi(message);
+				printf("Score du joueur %s: %d\n", g->nom[joueur], g->score[joueur]); 
+			up(semid);
+			break;
+
 		default:
 			printf("Message inconnu: %s\n", message);
+			return -1;
 	}
 
 	return 0;
